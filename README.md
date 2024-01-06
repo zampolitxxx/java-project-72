172a15e2564f632e8eef6cb6804d0aef5758f4c2
zampolitxxx <zampolitxxx@gmail.com>
2024-01-06 15:12:29 +0900

    second commit

--- README.md
+++
@@ -1,2 +0,0 @@
-### Hexlet tests and linter status:
-[![Actions Status](https://github.com/zampolitxxx/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/zampolitxxx/java-project-72/actions)
--- app/src/test/java/hexlet/code/MainTest.java
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -1,2 +1,12 @@
-package hexlet.code;public class MainTest {
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MainTest {
+    @Test
+    void Test(){
+        Assertions.assertFalse(1 == 2);
+    }
+
 }
