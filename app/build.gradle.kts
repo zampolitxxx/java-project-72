b915958150335f660f032f5a9664976008a1439d
zampolitxxx <zampolitxxx@gmail.com>
2024-01-14 17:23:33 +0900

    add first test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,13 @@ dependencies {
     implementation("com.h2database:h2:2.2.224")
     implementation("io.javalin:javalin:5.6.3")
     implementation("io.javalin:javalin-rendering:5.6.3")
+    implementation("io.javalin:javalin-bundle:5.6.3")
     implementation("org.slf4j:slf4j-simple:2.0.10")
     implementation("gg.jte:jte:3.1.6")
 
+    //tests
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/org.projectlombok/lombok
--- app/src/test/java/hexlet/code/MainTest.java
+++ app/src/test/java/hexlet/code/MainTest.java
@@ -1,12 +1,127 @@
 package hexlet.code;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlRepository;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
 
 public class MainTest {
-    @Test
-    void Test(){
-        Assertions.assertFalse(1 == 2);
+
+    Javalin app;
+
+    @BeforeEach
+    public final void setUp() throws IOException, SQLException {
+        app = App.getApp();
     }
 
+    @Test
+    public void testMainPage() {
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("Анализатор страниц");
+        });
+    }
+
+//    @Test
+//    public void testBuildSessionPage() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/sessions/build");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testUsersPage() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/users");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testCoursesPage() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/courses");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testBuildCourse() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/courses/build");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testCreateCourse() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var requestBody = "name=coursename&desdcription=coursedescription";
+//            var response = client.post("/courses", requestBody);
+//            assertThat(response.code()).isEqualTo(200);
+//            assertThat(response.body().string()).contains("coursename");
+//        });
+//    }
+//
+//    @Test
+//    public void testPostsPage() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/posts");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testCarsPage() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/cars");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testBuildCarPage() {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/cars/build");
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    public void testCarPage() throws SQLException {
+//        var car = new Url("honda", "accord");
+//        UrlRepository.save(car);
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/cars/" + car.getId());
+//            assertThat(response.code()).isEqualTo(200);
+//        });
+//    }
+//
+//    @Test
+//    void testCarNotFound() throws Exception {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/cars/999999");
+//            assertThat(response.code()).isEqualTo(404);
+//        });
+//    }
+//
+//    @Test
+//    void testUserNotFound() throws Exception {
+//        JavalinTest.test(app, (server, client) -> {
+//            var response = client.get("/users/999999");
+//            assertThat(response.code()).isEqualTo(404);
+//        });
+//    }
 }
