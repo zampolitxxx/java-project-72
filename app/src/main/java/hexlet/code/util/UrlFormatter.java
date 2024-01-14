b11656c512e9897974b3728a9d92b96692c8773d
zampolitxxx <zampolitxxx@gmail.com>
2024-01-14 15:51:43 +0900

    add urlformatter

--- app/src/main/java/hexlet/code/util/UrlFormatter.java
+++ app/src/main/java/hexlet/code/util/UrlFormatter.java
@@ -1,4 +1,28 @@
 package hexlet.code.util;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class UrlFormatter {
+    public static String formatURL(String url) throws URISyntaxException {
+        var uri = new URI(url).normalize();
+        try {
+            uri.toURL();
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException();
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append(uri.getScheme());
+        builder.append("://");
+        builder.append(uri.getHost());
+        var port = uri.getPort();
+
+        if(port != -1) {
+            builder.append(":");
+            builder.append(port);
+        }
+        return builder.toString();
+    }
+
 }
