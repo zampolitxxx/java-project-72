5c03b9754cf8fcb94efa0f55fd54619bab0685c8
zampolitxxx <zampolitxxx@gmail.com>
2024-01-30 20:46:46 +0900

    add string for local DB
    in getDatabaseUrl method add string for local postgresql db connection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -35,6 +35,7 @@ public class App {
     private static String getDatabaseUrl() {
         return System.getenv()
                 .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+//        return "jdbc:postgresql://localhost:5432/hexlet?password=1234567&user=zampolit";
     }
 
 
